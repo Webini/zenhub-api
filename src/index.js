72e38ba8ff359209bf86e731750305daadecd71d
James Hewitt-Thomas <james.hewitt@gmail.com>
2018-08-04 16:32:30 +0100

    Add support for enterprise ZenHub, use mocking for tests

--- src/index.js
+++ src/index.js
@@ -1,90 +1,91 @@
 const request = require('request-promise-native');
 const debug   = require('debug')('ZenHub');
+const url     = require('url');
 
-const API_URL = 'https://api.zenhub.io/p1';
+const PUBLIC_API_ENDPOINT = 'https://api.zenhub.io';
 
 const API_ENDPOINTS = {
   getIssueData: {
     method: 'GET',
-    path: '/repositories/:repo_id/issues/:issue_number',
+    path: 'p1/repositories/:repo_id/issues/:issue_number',
   },
   getIssueEvents: {
     method: 'GET',
-    path: '/repositories/:repo_id/issues/:issue_number/events',
+    path: 'p1/repositories/:repo_id/issues/:issue_number/events',
   },
   getBoard: {
     method: 'GET',
-    path: '/repositories/:repo_id/board',
+    path: 'p1/repositories/:repo_id/board',
   },
   getEpics: {
     method: 'GET',
-    path: '/repositories/:repo_id/epics',
+    path: 'p1/repositories/:repo_id/epics',
   },
   getEpicData: {
     method: 'GET',
-    path: '/repositories/:repo_id/epics/:epic_id',
+    path: 'p1/repositories/:repo_id/epics/:epic_id',
   },
   changePipeline: {
     method: 'POST',
-    path: '/repositories/:repo_id/issues/:issue_number/moves',
+    path: 'p1/repositories/:repo_id/issues/:issue_number/moves',
   },
   setEstimate: {
     method: 'PUT',
-    path: '/repositories/:repo_id/issues/:issue_number/estimate'
+    path: 'p1/repositories/:repo_id/issues/:issue_number/estimate'
   },
   convertToEpic: {
     method: 'POST',
-    path: '/repositories/:repo_id/issues/:issue_number/convert_to_epic',
+    path: 'p1/repositories/:repo_id/issues/:issue_number/convert_to_epic',
   },
   convertToIssue: {
     method: 'POST',
-    path: '/repositories/:repo_id/epics/:issue_number/convert_to_issue',
+    path: 'p1/repositories/:repo_id/epics/:issue_number/convert_to_issue',
   },
   addOrRemoveToEpic: {
     method: 'POST',
-    path: '/repositories/:repo_id/epics/:issue_number/update_issues'
+    path: 'p1/repositories/:repo_id/epics/:issue_number/update_issues'
   },
   createReleaseReport: {
     method: 'POST',
-    path: '/repositories/:repo_id/reports/release'
+    path: 'p1/repositories/:repo_id/reports/release'
   },
   getReleaseReport: {
     method: 'GET',
-    path: '/reports/release/:release_id'
+    path: 'p1/reports/release/:release_id'
   },
   getReleaseReportsForRepo: {
     method: 'GET',
-    path: '/repositories/:repo_id/reports/releases'
+    path: 'p1/repositories/:repo_id/reports/releases'
   },
   editReleaseReport: {
     method: 'PATCH',
-    path: '/reports/release/:release_id'
+    path: 'p1/reports/release/:release_id'
   },
   addWorkspaceToReleaseReport: {
     method: 'PATCH',
-    path: '/reports/release/:release_id/workspaces/add'
+    path: 'p1/reports/release/:release_id/workspaces/add'
   },
   removeWorkspaceFromReleaseReport: {
     method: 'PATCH',
-    path: '/reports/release/:release_id/workspaces/remove'
+    path: 'p1/reports/release/:release_id/workspaces/remove'
   },
   getReleaseReportIssues: {
     method: 'GET',
-    path: '/reports/release/:release_id/issues'
+    path: 'p1/reports/release/:release_id/issues'
   },
   addOrRemoveToReleaseReport: {
     method: 'PATCH',
-    path: '/reports/release/:release_id/issues'
+    path: 'p1/reports/release/:release_id/issues'
   }
 };
 
-function callServer(apiKey, params, endpoint) {
-  const uri = API_URL + endpoint.path
+function callServer(apiKey, apiUrl, params, endpoint) {
+  const uri = url.resolve(apiUrl, endpoint.path
     .replace(':repo_id', params.repo_id)
     .replace(':issue_number', params.issue_number)
     .replace(':epic_id', params.epic_id)
     .replace(':release_id', params.release_id)
-  ;
+  );
 
   debug('%s %s with %o', endpoint.method, uri, params);
 
@@ -99,13 +100,14 @@ function callServer(apiKey, params, endpoint) {
   });
 }
 
-function ZenHub(apiKey) {
+function ZenHub(apiKey, apiUrl = PUBLIC_API_ENDPOINT) {
   this.apiKey = apiKey;
+  this.apiUrl = apiUrl;
 };
 
 Object.keys(API_ENDPOINTS).forEach(function(value) {
   ZenHub.prototype[value] = function(params) {
-    return callServer(this.apiKey, params, API_ENDPOINTS[value]);
+    return callServer(this.apiKey, this.apiUrl, params, API_ENDPOINTS[value]);
   };
 });
 
